aa3526f706eee00014fe954658f59f518d785b7f
Russ Goetz <russgoetz@gmail.com>
2024-03-19 20:04:16 -0400

    Creating types for the header fields

--- dns/resolver/src/lib.rs
+++ dns/resolver/src/lib.rs
@@ -1,4 +1,14 @@
 use bytes::buf::{Buf, BufMut};
+use std::fmt;
+
+macro_rules! invalid_field_value {
+    ($field_name:literal, $value:expr) => {
+        Err(Error::InvalidFieldValue {
+            field_name: $field_name.to_string(),
+            value: $value,
+        })
+    }
+}
 
 pub fn encode_name(name: &str) -> Vec<u8> {
     let mut qname = Vec::new();
@@ -14,9 +24,9 @@ pub fn encode_name(name: &str) -> Vec<u8> {
 
 pub struct Header {
     pub id: u16,
-    pub qr: MessageType,
+    pub qr: QR,
+    pub opcode: Opcode,
     /*
-    pub opcode: u16 = 0; // standard query
     pub aa: u16 = 0; // authoritative answer: ignored in query
     pub tc: u16 = 0; // truncation: ignore in query
     pub rd: u16 = 0; // recursion not desired
@@ -30,48 +40,118 @@ pub struct Header {
     */
 }
 
-pub enum MessageType {
+#[derive(Debug)]
+pub enum QR {
     Query,
     Response,
 }
 
-impl MessageType {
-    pub fn to_int(&self) -> u16 {
+impl QR {
+    const POS: usize = 15;
+    const WIDTH: u16 = 1;
+
+    pub fn build(&self) -> u16 {
         match self {
-            MessageType::Query => 0,
-            MessageType::Response => 1,
+            QR::Query => 0,
+            QR::Response => 1 << QR::POS,
         }
     }
 
-    pub fn from_int(num: u16) -> Result<MessageType> {
-        match num {
-            0 => Ok(MessageType::Query),
-            1 => Ok(MessageType::Response),
-            _ => invalid_field_value!(
-                    "
+    pub fn parse(word: u16) -> QR {
+        match (word >> QR::POS) & QR::WIDTH  {
+            0 => QR::Query,
+            1 => QR::Response,
+            _ => QR::Query, // Not possible
         }
     }
 }
 
+#[derive(Debug)]
+pub enum Opcode {
+    Query,
+    IQuery,
+    Status,
+}
+
+impl Opcode {
+    const POS: usize = 11;
+    const WIDTH: u16 = 4;
+
+    pub fn build(&self) -> u16 {
+        match self {
+            Opcode::Query => 0,
+            Opcode::IQuery => 1 << Opcode::POS,
+            Opcode::Status => 2 << Opcode::POS,
+         }
+    }
+
+    pub fn parse(word: u16) -> Result<Opcode> {
+        match (word >> Opcode::POS) & Opcode::WIDTH {
+            0 => Ok(Opcode::Query),
+            1 => Ok(Opcode::IQuery),
+            2 => Ok(Opcode::Status),
+            val => invalid_field_value!("Opcode", val),
+        }
+    }
+}
+
+// TODO: Impl default for all these enums.
+#[derive(Debug)]
+pub enum AA {
+    NonAuthoritative,
+    Authoritative,
+}
+
+impl AA {
+    const POS: usize = 15;
+    const WIDTH: u16 = 1;
+
+    pub fn build(&self) -> u16 {
+        match self {
+            QR::Query => 0,
+            QR::Response => 1 << QR::POS,
+        }
+    }
+
+    pub fn parse(word: u16) -> QR {
+        match (word >> QR::POS) & QR::WIDTH  {
+            0 => QR::Query,
+            1 => QR::Response,
+            _ => QR::Query, // Not possible
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum Error {
     InvalidFieldValue {
         field_name: String,
-        message: String,
+        value: u16,
     },
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::InvalidFieldValue { field_name, value } => {
+                write!(f, "{value} is an invalid value for the '{field_name}' field")
+            },
+        }
+    }
+    
 }
 
 impl std::error::Error for Error {}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-macro_rules! invalid_field_value(
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn encode_name1() {
+    fn encode_name_simple() {
         let encoded = encode_name("google.com");
 
         let mut expected = Vec::new();
@@ -83,4 +163,14 @@ mod test {
 
         assert_eq!(encoded, expected);
     }
+
+    #[test]
+    fn build_qr() {
+        let query = QR::Query;
+        assert_eq!(query.build(), 0);
+
+        let response = QR::Response;
+        assert_eq!(query.build(), 0x8000);
+    }
+
 }
