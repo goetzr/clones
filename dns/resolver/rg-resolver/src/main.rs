7d7c02efe4aac2a76071073230aab2f6af765721
Russ Goetz <russgoetz@gmail.com>
2024-07-05 11:38:59 -0400

    WIP

---
+++ dns/resolver/rg-resolver/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use tracing::info;
+use tracing_subscriber;
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("ERROR: {e}");
+        std::process::exit(1);
+    }
+}
+
+fn run() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    
+    let Some(domain_name) = env::args().skip(1).next() else {
+        anyhow::bail!("must specify domain name".to_string());
+    };
+    info!("Querying address(es) for domain name {domain_name}...");
+
+    Ok(())
+}
