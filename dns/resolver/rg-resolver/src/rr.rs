65a716542ed7df0f6ac343ded05b5eed55897b7e
Russ Goetz <russgoetz@gmail.com>
2024-07-05 20:51:08 -0400

    WIP

--- dns/resolver/rg-resolver/src/rr.rs
+++ dns/resolver/rg-resolver/src/rr.rs
@@ -1,5 +1,6 @@
-use bytes::Buf;
+use bytes::{Buf, BufMut};
 use anyhow::Context;
+use tracing::{instrument, debug};
 
 pub struct ResourceRecord {
     name: String,
@@ -43,6 +44,7 @@ impl<'a> ResourceRecordParser<'a> {
     }
 }
 
+#[derive(Debug)]
 struct ResourceRecordNameParser<'a> {
     data: &'a [u8],
     unparsed: &'a [u8],
@@ -53,18 +55,27 @@ impl<'a> ResourceRecordNameParser<'a> {
         Self { data, unparsed: data }
     }
 
+    //#[instrument(level = "debug")]
     fn parse(&mut self) -> anyhow::Result<String> {
+        debug!("Called parse");
         let mut name = String::new();
         loop {
             let len = if self.unparsed.has_remaining() {
                 let mut peek = self.unparsed;
                 peek.get_u8() as usize
             } else {
-                anyhow::bail!("malformed name")
+                anyhow::bail!("incomplete name")
             };
+            debug!(len, "Parsed length byte");
+            use std::io::Write;
+            let mut stdout = std::io::stdout();
+            stdout.flush();
             if len == 0 {
-                name.push('.');
-                return Ok(name);
+                if name.len() <= 255 {
+                    return Ok(name);
+                } else {
+                    anyhow::bail!("name exceeds maximum length of 255");
+                }
             }
             if Self::is_compressed(len)? {
                 let offset = if self.unparsed.remaining() >= 2 {
@@ -73,19 +84,23 @@ impl<'a> ResourceRecordNameParser<'a> {
                     anyhow::bail!("incomplete pointer")
                 };
                 self.unparsed = &self.data[offset..];
+                // Continue parsing the name starting at the pointed to location.
                 continue;
             }
             let label = if self.unparsed.remaining() >= len {
-                // TODO: Need to make sure it's all ASCII too
-                String::from_utf8(self.unparsed[..len].to_vec()).map(|e| e.with_context(|| ")
-            }
-            if self.unparsed.remaining() < len {
-                anyhow::bail!("incomplete name");
-            }
-
+                let label = String::from_utf8(self.unparsed[..len].to_vec())
+                    .with_context(|| "label not valid UTF-8")?;
+                if label.is_ascii() {
+                    label
+                } else {
+                    anyhow::bail!("label not ASCII");
+                }
+            } else {
+                anyhow::bail!("incomplete label")
+            };
+            name.push_str(&label);
+            name.push('.');
         }
-        
-        // Total length of name (label octets + label length octets) must be <= 255
     }
 
     fn is_compressed(len: usize) -> anyhow::Result<bool> {
@@ -121,4 +136,47 @@ pub enum Class {
     CS,
     CH,
     HS,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use bytes::BufMut;
+    use tracing_test::traced_test;
+
+    fn serialize_name(name: &str) -> Vec<u8> {
+        let mut buf = Vec::new();
+        let labels = name.split('.').collect::<Vec<_>>();
+        for label in labels {
+            buf.put_u8(label.len() as u8);
+            for b in label.chars().map(|c| c as u8) {
+                buf.put_u8(b);
+            }
+        }
+        buf
+    }
+
+    #[test]
+    fn serialize_name_helper() {
+        let serialized = serialize_name("mail.google.com.");
+        let expected: [u8; 17] = [4, b'm', b'a', b'i', b'l', 6, b'g', b'o', b'o', b'g', b'l', b'e', 3, b'c', b'o', b'm', 0];
+        assert_eq!(serialized.as_slice(), &expected);
+    }
+
+    #[traced_test]
+    #[test]
+    fn parse_name() -> anyhow::Result<()> {
+        //tracing_subscriber::fmt::init();
+        debug!("Running test");
+        let data = serialize_name("mail.google.com.");
+        let mut parser = ResourceRecordNameParser::new(data.as_slice());
+        let name = parser.parse()?;
+        assert_eq!(name, "mail.google.com.");
+        Ok(())
+    }
+
+    #[test]
+    fn parse_compressed_name() -> anyhow::Result<()> {
+        Ok(())
+    }
 }
