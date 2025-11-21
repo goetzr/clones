5c65d4c9c0208cac889e015972d1d31cc14d2048
Russ Goetz <russgoetz@gmail.com>
2025-11-20 20:30:53 -0500

    Working on sending a GET request using libcurl

--- docker/src/http.cpp
+++ docker/src/http.cpp
@@ -5,32 +5,32 @@ Curl::Curl()
 {
     CURLcode result = curl_global_init(CURL_GLOBAL_NOTHING);
     if (result != CURLE_OK) {
-        throw new CurlError(result);
+        throw new CurlError("curl_global_init failed", result);
     }
 
     CURL* handle = curl_easy_init();
     if (handle == nullptr) {
-        throw new CurlError("curl_easy_init failed");
+        throw new CurlErrorBase("curl_easy_init failed");
     }
-
-    
 }
 
-std::string Curl::get(const std::string& url) {
-    // TODO: Code error should include string description.
+std::string Curl::get(const std::string& url, const HeaderMap& headers) {
+    // CURLcode curl_easy_setopt(CURL *handle, CURLOPT_HTTPGET, long useget);
+
     CURLcode result = curl_easy_setopt(m_handle, CURLOPT_URL, url.c_str());
     if (result != CURLE_OK) {
-        throw new CurlError(result);
+        throw new CurlError("curl_easy_setopt (CURLOPT_URL) failed", result);
     }
 
     std::string response;
     response.reserve(1024 * 1024);
     result = curl_easy_setopt(m_handle, CURLOPT_WRITEFUNCTION, [&response] (
         char* ptr,
-        size_t size,
+        size_t _size,
         size_t nmemb,
         void* _userdata
     ) -> size_t {
+        // Documentation states this function may be called with 0 bytes if the transferred file is empty.
         if (nmemb != 0) {
             response.append(ptr, nmemb);
         }
@@ -38,15 +38,35 @@ std::string Curl::get(const std::string& url) {
         return nmemb;
     });
     
+    // TODO: Pick up here.
+    // CURLcode curl_easy_setopt(CURL *handle, CURLOPT_HTTPHEADER,
+                          // struct curl_slist *headers);
+    CurlStringList headers;
+    for (const auto& [key, value] : headers) {
+        std::ostringstream header;
+        header << key << ": " << value;
+        headers.append(header);
+    }
+    result = curl_easy_setopt(m_hanlde, CURLOPT_HTTPHEADER, headers.native());
+    if (result != CURLE_OK) {
+        throw new CurlError("curl_easy_setopt (CURLOPT_HTTPHEADER) failed");
+    }
+
+    // success = curl_easy_perform(handle);
+}
+
+std::ostream& operator>>(std::ostream& os, const CurlErrorBase& error) {
+    os << "CURL: " << error.message();
 }
 
 std::ostream& operator>>(std::ostream& os, const CurlError& error) {
-    switch(error.kind) {
-        case CurlErrorKind::Code:
-            os << "error code: " << error.code;
-            break;
-        case CurlErrorKind::Message:
-            os << error.message;
-            break;
-    };
+    os << "CURL:" << error.message() << ", error code = " << error.code();
+}
+
+void CurlStringList::append(const std::string& value) {
+    curl_slist* new_list = curl_slist_append(m_slist, value.c_str());
+    if (new_list == nullptr) {
+        throw new CurErrorBase("curl_slist_append failed");
+    }
+    m_slist = new_list;
 }
--- docker/src/http.h
+++ docker/src/http.h
@@ -6,32 +6,51 @@
 
 #include "pch.h"
 
+using HeaderMap = std::unordered_map<std::string, std::string>;
+
 class Curl {
 public:
     Curl();
     ~Curl() { curl_global_cleanup(); }
-    std::string get(const std::string& url);
+
+    std::string get(const std::string& url, const HeaderMap& headers);
 
 private:
     CURL* m_handle;
 };
 
-class CurlError {
+class CurlErrorBase {
 public:
-    CurlError(const std::string& message)
-        : message{message},
-          code{std::nullopt}
+    explicit CurlErrorBase(const std::string& message)
+        : m_message{message}
     {}
 
-    CurlError(const std::string& message, CURLcode code)
-        : message{message},
-          code{code}
-    {}
+    const std::string& message() const { return m_message; }
 
-private:
-    std::string message;
-    std::optional<CURLcode> code;
-
-    friend std::ostream& operator>>(std::ostream& os, const CurlError& error);
+protected:
+    std::string m_message;
 };
 
+class CurlError : public CurlErrorBase {
+public:
+    CurlError(const std::string& message, CURLcode code)
+        : CurlErrorBase{message},
+          m_code{code}
+    {}
+
+    CURLcode code() const { return m_code; }
+
+private:
+    CURLcode m_code;
+};
+
+class CurlStringList {
+public:
+    CurlStringList() : m_slist{nullptr} {};
+    ~CurlStringList() { curl_slist_free_all(m_slist); m_slist = nullptr; }
+
+    void append(const std::string& value);
+    curl_slist* native() const { return m_slist; }
+private:
+    curl_slist* m_slist;
+};
